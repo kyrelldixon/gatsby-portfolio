8ef929cce8e13906d35aba821cdbe357fe37a0c4
Kyrell Dixon <kyrell.brown@gmail.com>
2018-01-09 17:13:14 -0600

    Added Header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,53 @@
 import React from 'react';
-// import Link from 'gatsby-link';
+import styled from 'styled-components';
+import GatsbyLink from 'gatsby-link';
+
+const HeaderWrapper = styled.header``;
+const HeaderTop = styled.div``;
+const Logo = styled.h1``;
+const HeaderDetails = styled.p``;
+const Nav = styled.nav``;
+const NavItems = styled.ul``;
+const NavItem = styled.li``;
+const HeaderBottom = styled.div``;
+const Email = styled.span``;
+const Link = styled(GatsbyLink)``;
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <HeaderWrapper>
+    <HeaderTop>
+      <Logo>Logo</Logo>
+      <HeaderDetails>
+        Front-end Developer<br/>
+        Growth Hacker
+      </HeaderDetails>
+    </HeaderTop>
+    <Nav>
+      <NavItems>
+        <NavItem>
+          <Link to="/">Home</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/">Services</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/">Portfolio</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/">Blog</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/">Contact</Link>
+        </NavItem>
+      </NavItems>
+    </Nav>
+    <HeaderBottom>
+      <HeaderDetails>
+        For more info or to see if my services can help your business, you can contact me at:
+      </HeaderDetails>
+      <Email>hello@kyrelldixon.com</Email>
+    </HeaderBottom>
+  </HeaderWrapper>
 )
 
 export default Header;
