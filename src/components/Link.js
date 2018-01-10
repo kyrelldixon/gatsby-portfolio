a6dc2395b4a285a849f4d1614901ab9c386866d1
Kyrell Dixon <kyrell.brown@gmail.com>
2018-01-10 00:35:13 -0600

    Went back to basic link style

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,34 +1,51 @@
-import React from 'react';
 import styled from 'styled-components';
-import UnstyledLink from 'gatsby-link';
+import GatsbyLink from 'gatsby-link';
 
-export const StyledLink = styled.a`
-  display: inline-block;
+const Link = styled(GatsbyLink) `
   text-decoration: none;
   color: ${props => props.active ? 'rgba(0,0,0,0.9)' : 'inherit'};
   border-bottom: ${props => props.active ? '2px solid black' : 'none'};
   transition: border-color .1s,color .1s;
-  cursor: pointer;
-  
+
   &:hover {
     color: rgba(0,0,0,0.9);
     border-bottom: 2px solid black;
   }
-`
+`;
 
-const Link = ({ children, className, unstyled, ...rest }) => {
-  let Child = StyledLink;
-  if (unstyled) {
-    Child = 'a';
-  }
+export default Link;
 
-  return (
-    <UnstyledLink {...rest}>
-      <Child href={rest.href} className={className}>
-        {children}
-      </Child>
-    </UnstyledLink>
-  )
-}
+// import React from 'react';
+// import styled from 'styled-components';
+// import UnstyledLink from 'gatsby-link';
 
-export default Link
+// export const StyledLink = styled.a`
+//   display: inline-block;
+//   text-decoration: none;
+//   color: ${props => props.active ? 'rgba(0,0,0,0.9)' : 'inherit'};
+//   border-bottom: ${props => props.active ? '2px solid black' : 'none'};
+//   transition: border-color .1s,color .1s;
+//   cursor: pointer;
+  
+//   &:hover {
+//     color: rgba(0,0,0,0.9);
+//     border-bottom: 2px solid black;
+//   }
+// `
+
+// const Link = ({ children, className, unstyled, ...rest }) => {
+//   let Child = StyledLink;
+//   if (unstyled) {
+//     Child = 'a';
+//   }
+
+//   return (
+//     <UnstyledLink {...rest}>
+//       <Child href={rest.href} className={className}>
+//         {children}
+//       </Child>
+//     </UnstyledLink>
+//   )
+// }
+
+// export default Link;
