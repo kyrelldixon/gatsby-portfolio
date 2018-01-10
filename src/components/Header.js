e0d85806d30e2ace44e734fde2098cb94b8c2d72
Kyrell Dixon <kyrell.brown@gmail.com>
2018-01-09 22:51:44 -0600

    Moved Link and Nav to own file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import GatsbyLink from 'gatsby-link';
+
+import Nav from './Nav';
 
 const HeaderWrapper = styled.header`
   display: flex;
@@ -25,32 +26,10 @@ const HeaderDetails = styled.p`
   line-height: 1.6em;
   margin-bottom: 1.4em;
 `;
-const Nav = styled.nav`
-  flex-basis: 50%;
-`;
-const NavItems = styled.ul`
-  margin: 0;
-`;
-const NavItem = styled.li`
-  list-style: none;
-  margin: 20px 0;
-  font-size: 1.2em;
-  text-transform: uppercase;
-  color: rgba(0,0,0,0.65);
-  transition: border-color .1s,color .1s;
-`;
 const HeaderBottom = styled.div`
   flex-basis: 25%;
 `;
 const Email = styled.span``;
-const Link = styled(GatsbyLink)`
-  text-decoration: none;
-  color: inherit;
-  &:hover {
-    color: rgba(0,0,0,0.9);
-    border-bottom: 2px solid black;
-  }
-`;
 
 const Header = () => (
   <HeaderWrapper>
@@ -62,25 +41,7 @@ const Header = () => (
         Some Third Detail About Me
       </HeaderDetails>
     </HeaderTop>
-    <Nav>
-      <NavItems>
-        <NavItem>
-          <Link to="/">Home</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/page-2">Services</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/">Portfolio</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/">Blog</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/">Contact</Link>
-        </NavItem>
-      </NavItems>
-    </Nav>
+    <Nav />
     <HeaderBottom>
       <HeaderDetails>
         For more info or to see if my services can help your business, you can contact me at:
