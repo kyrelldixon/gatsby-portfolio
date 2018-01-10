2f197a66f639458bde78ce052c998aa132e0b764
Kyrell Dixon <kyrell.brown@gmail.com>
2018-01-10 00:31:53 -0600

    Updated link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,16 +1,34 @@
+import React from 'react';
 import styled from 'styled-components';
-import GatsbyLink from 'gatsby-link';
+import UnstyledLink from 'gatsby-link';
 
-const Link = styled(GatsbyLink) `
+export const StyledLink = styled.a`
+  display: inline-block;
   text-decoration: none;
   color: ${props => props.active ? 'rgba(0,0,0,0.9)' : 'inherit'};
   border-bottom: ${props => props.active ? '2px solid black' : 'none'};
   transition: border-color .1s,color .1s;
-
+  cursor: pointer;
+  
   &:hover {
     color: rgba(0,0,0,0.9);
     border-bottom: 2px solid black;
   }
-`;
+`
 
-export default Link;
+const Link = ({ children, className, unstyled, ...rest }) => {
+  let Child = StyledLink;
+  if (unstyled) {
+    Child = 'a';
+  }
+
+  return (
+    <UnstyledLink {...rest}>
+      <Child href={rest.href} className={className}>
+        {children}
+      </Child>
+    </UnstyledLink>
+  )
+}
+
+export default Link
