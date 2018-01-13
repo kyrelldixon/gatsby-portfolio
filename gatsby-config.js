82ab6bf34af00948f9a2a3aec96ad4f959d4ab92
Kyrell Dixon <kyrell.brown@gmail.com>
2018-01-12 23:03:55 -0600

    Added contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || ``,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ``,
+        host: ``
+      }
+    }
   ],
 }
